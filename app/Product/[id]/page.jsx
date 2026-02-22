23e4453b6939a24a5943b72be19ab7aa9fc28279
Hastingamo <hezekiahhastings14@gmail.com>
2026-02-22 23:19:53 +0100

    first commit

---
+++ app/Product/[id]/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='w-full flex flex-row bg-[] '> 
+        <div className="bg-[#8487bf] w-3/4 h-screen text-white font-bold">
+            <h1>Crypto page</h1>
+        </div>
+        <div className="bg-[#f0f1f2] w-1/4 h-screen text-black font-bold">
+
+        </div>
+
+    </div>
+  )
+}
+
+export default page
--- app/Product/page.jsx
+++ app/Product/page.jsx
@@ -28,53 +28,88 @@ function Page() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    const filtered = data.filter((item) => {
+      return (
+        item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        item.symbol.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    });
+    setFilteredData(filtered);
+    if (!data) {
+      setFilteredData([]);
+      return(
+        <div className="flex items-center justify-center h-screen">
+          <p className="text-2xl font-bold text-red-500">No data available</p>
+        </div>
+      )
+
+    }
+  }, [searchTerm, data]);
   return (
-    <div className="bg-[#1B3358] w-full   text-white  font-bold">
+    <div className="bg-[#1B3358] w-full  h-full text-white  font-bold">
+      <div className="grid grid-cols-1 md:grid-cols-2">
       <h1 className="ml-10 pt-5">Crypto page</h1>
+      <input
+        type="text"
+        placeholder="Search by name or symbol"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className=" mt-5 p-2 border-2 w-3/4 ml-5 xl:w-1/2 xl:ml-[20rem] rounded-lg text-black"
+      />
+      </div>
+
       {loading ? (
         <p>Loading...</p>
       ) : (
         <motion.div
-            initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.5 }}
-
-               
-                viewport={{once:true}}
-                whileInView={{opacity: 1, y:0}}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          viewport={{ once: true }}
+          whileInView={{ opacity: 1, y: 0 }}
         >
-                   <div className="grid grid-cols-1 ">
-            {data.map((item, index) => (
+          <div className="grid grid-cols-1 ">
+            {filteredData.map((item, index) => (
               <motion.div
-                initial={{ opacity: 0, x:-30  }}
+                initial={{ opacity: 0, x: -30 }}
                 animate={{ opacity: 1, x: 5 }}
                 transition={{ duration: 3 }}
                 whileHover={{ scale: 1.02 }}
                 key={index}
-                whileInView={{opacity: 1, y:0}}
-                className="lg:text-[18px]">
-                    <Link href={`/Product/${item.id}`} className="m-4 p-6 border-2 bg-gradient-to-br from-[#1B3358] to-[#06142E] xl:ml-[2rem] rounded-2xl xl:rounded-[10px] xl:pt-4 xl:pb-4 grid  md:grid-cols-2 lg:p-0 lg:grid-cols-4 xl:grid-cols-6">
-                     <Image src={item.image} alt={item.name} width={50} height={50} />
-                <p>
-                  #{index + 1} {item.name} ({item.symbol.toUpperCase()} || {item.category})
-                </p>
-                <p>💰 Price: ${item.current_price.toLocaleString()}</p>
-                <p>📈 Market Cap: ${item.market_cap.toLocaleString()}</p>
-                <p
-                  className={`font-semibold ${
-                    (item.price_change_percentage_24h ?? 0) > 0
-                      ? "text-green-500"
-                      : "text-red-500"
-                  }`}
+                whileInView={{ opacity: 1, y: 0 }}
+                className="lg:text-[18px]"
+              >
+                <Link
+                  href={`/Product/${item.id}`}
+                  className="m-4 p-6 border-2 bg-gradient-to-br from-[#1B3358] to-[#06142E] xl:ml-[2rem] rounded-2xl xl:rounded-[10px] xl:pt-4 xl:pb-4 grid  md:grid-cols-2 lg:p-0 lg:grid-cols-4 xl:grid-cols-6"
                 >
-                  1min Change: {typeof item.price_change_percentage_24h === "number"
-                    ? `${item.price_change_percentage_24h.toFixed(2)}%`
-                    : "N/A"}
-                </p>
-                                <p>🔄 Volume: ${item.total_volume.toLocaleString()}</p>
-         
-                    </Link>
-               
+                  <Image
+                    src={item.image}
+                    alt={item.name}
+                    width={50}
+                    height={50}
+                  />
+                  <p>
+                    #{index + 1} {item.name} ({item.symbol.toUpperCase()} ||{" "}
+                    {item.category})
+                  </p>
+                  <p>💰 Price: ${item.current_price.toLocaleString()}</p>
+                  <p>📈 Market Cap: ${item.market_cap.toLocaleString()}</p>
+                  <p
+                    className={`font-semibold ${
+                      (item.price_change_percentage_24h ?? 0) > 0
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }`}
+                  >
+                    1min Change:{" "}
+                    {typeof item.price_change_percentage_24h === "number"
+                      ? `${item.price_change_percentage_24h.toFixed(2)}%`
+                      : "N/A"}
+                  </p>
+                  <p>🔄 Volume: ${item.total_volume.toLocaleString()}</p>
+                </Link>
               </motion.div>
             ))}
           </div>
