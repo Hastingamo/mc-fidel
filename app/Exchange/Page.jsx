712aebb5d7e3993155ba5be582f81d2a78995e22
Hastingamo <hezekiahhastings14@gmail.com>
2026-03-13 10:45:16 +0100

    ttttt

--- app/Exchange/Page.jsx
+++ app/Exchange/Page.jsx
@@ -1,213 +1,4 @@
-// "use client";
 
-// import React, { useEffect, useState } from "react";
-// import { motion } from "framer-motion";
-
-// function Page() {
-//   const [currency, setCurrency] = useState("");
-//   const [fromThisCurrency, setFromThisCurrency] = useState("");
-//   const [toThisCurrency, setToThisCurrency] = useState("");
-//   const [amount, setAmount] = useState("");
-//   const [coins, setCoins] = useState([]);
-//   const [error, setError] = useState("");
-//   const [activeTab, setActiveTab] = useState("sell");
-
-//   const phone = "2348036210152";
-
-//   useEffect(() => {
-//     const fetchCoins = async () => {
-//       try {
-//         const res = await fetch("https://api.coingecko.com/api/v3/coins/list");
-//         const data = await res.json();
-//         setCoins(data);
-//       } catch (err) {
-//         setError("Failed to fetch coin data");
-//       }
-//     };
-
-//     fetchCoins();
-//   }, []);
-
-//   const validateCurrency = () => {
-//     const supportedCoins = coins.map((coin) => coin.symbol.toUpperCase());
-
-//     if (!supportedCoins.includes(currency.toUpperCase())) {
-//       setError("Sorry, we do not support that currency.");
-//       return false;
-//     }
-
-//     setError("");
-//     return true;
-//   };
-
-//   const openWhatsApp = (text) => {
-//     const message = encodeURIComponent(text);
-//     window.open(`https://wa.me/${phone}?text=${message}`, "_blank");
-//   };
-
-//   const handleBuy = () => {
-//     if (!validateCurrency()) return;
-
-//     openWhatsApp(`Hello, I want to buy ${amount} ${currency}`);
-//   };
-
-//   const handleSell = () => {
-//     if (!validateCurrency()) return;
-
-//     openWhatsApp(`Hello, I want to sell ${amount} ${currency}`);
-//   };
-
-//   const handleSwap = () => {
-//     if (!validateCurrency()) return;
-
-//     openWhatsApp(`Hello, I want to exchange ${amount} ${currency}`);
-//   };
-
-//   const Form = ({ buttonText, action }) => (
-//     <form
-//       onSubmit={(e) => {
-//         e.preventDefault();
-//         action();
-//       }}
-//       className="space-y-5"
-//     >
-//       {error && <p className="text-red-500 text-lg">{error}</p>}
-
-//       <div>
-//         <label className="block text-lg font-medium">Currency</label>
-//         <input
-//           type="text"
-//           value={currency}
-//           onChange={(e) => setCurrency(e.target.value)}
-//           className="w-full border rounded-lg p-2 mt-1"
-//           required
-//         />
-//       </div>
-
-//       <div>
-//         <label className="block text-lg font-medium">Amount</label>
-//         <input
-//           type="number"
-//           value={amount}
-//           onChange={(e) => setAmount(e.target.value)}
-//           className="w-full border rounded-lg p-2 mt-1"
-//           required
-//         />
-//       </div>
-
-//       <button
-//         type="submit"
-//         className="bg-green-500 text-white px-6 py-3 rounded-lg w-full"
-//       >
-//         {buttonText}
-//       </button>
-//     </form>
-//   );
-
-//   const renderTabContent = () => {
-//     switch (activeTab) {
-//       case "buy":
-//         return <Form buttonText="Buy on WhatsApp" action={handleBuy} />;
-
-//       case "swap":
-//         return (
-//           <>
-//             <form
-//               onSubmit={(e) => {
-//                 e.preventDefault();
-//               }}
-//               className="space-y-5"
-//             >
-//               {error && <p className="text-red-500 text-lg">{error}</p>}
-
-//               <div>
-//                 <label className="block text-lg font-medium">
-//                   {" "}
-//                   from this Currency
-//                 </label>
-//                 <input
-//                   type="text"
-//                   value={fromThisCurrency}
-//                   onChange={(e) => setFromThisCurrency(e.target.value)}
-//                   className="w-full border rounded-lg p-2 mt-1"
-//                   required
-//                 />
-//               </div>
-
-//               <div>
-//                 <label className="block text-lg font-medium">
-//                   to this currency
-//                 </label>
-//                 <input
-//                   type="text"
-//                   value={toThisCurrency}
-//                   onChange={(e) => setToThisCurrency(e.target.value)}
-//                   className="w-full border rounded-lg p-2 mt-1"
-//                   required
-//                 />
-//               </div>
-
-//               <button
-//                 type="submit"
-//                 className="bg-green-500 text-white px-6 py-3 rounded-lg w-full"
-//               >
-//                SWAP
-//               </button>
-//             </form>
-//           </>
-//         );
-
-//       default:
-//         return <Form buttonText="Sell on WhatsApp" action={handleSell} />;
-//     }
-//   };
-
-//   return (
-//     <motion.div
-//       initial={{ opacity: 0, y: 50, scale: 0.9 }}
-//       animate={{ opacity: 1, y: 0, scale: 1 }}
-//       transition={{ duration: 0.8 }}
-//       className="bg-gradient-to-b from-[#0F2D27] to-[#2c5364]  w-full min-h-screen p-4 font-bold"
-//     >
-//       <h1 className="text-3xl mb-6">Exchange Page</h1>
-
-//       <div className="w-full md:w-3/4 mx-auto border bg-[#1B3358] shadow-lg shadow min-h-[70%] grid md:grid-cols-2">
-//         <div className="flex md:flex-col justify-around md:justify-start gap-4 p-5">
-//           <button
-//             onClick={() => setActiveTab("sell")}
-//             className={`px-4 py-2 rounded-lg ${
-//               activeTab === "sell" ? "bg-gray-300" : "bg-white"
-//             }`}
-//           >
-//             Sell
-//           </button>
-
-//           <button
-//             onClick={() => setActiveTab("buy")}
-//             className={`px-4 py-2 rounded-lg ${
-//               activeTab === "buy" ? "bg-gray-300" : "bg-white"
-//             }`}
-//           >
-//             Buy
-//           </button>
-
-//           <button
-//             onClick={() => setActiveTab("swap")}
-//             className={`px-4 py-2 rounded-lg ${
-//               activeTab === "swap" ? "bg-gray-300" : "bg-white"
-//             }`}
-//           >
-//             Exchange
-//           </button>
-//         </div>
-
-//         <div className="p-5">{renderTabContent()}</div>
-//       </div>
-//     </motion.div>
-//   );
-// }
-
-// export default Page;
 "use client";
 
 import React, { useEffect, useState } from "react";
@@ -336,7 +127,7 @@ function Page() {
 
       <button
         type="submit"
-        className="bg-primary hover:bg-primary/90 text-white font-bold px-6 py-4 rounded-xl w-full shadow-lg transition-all active:scale-[0.98]"
+        className="bg-primary text-white font-bold px-6 py-4 rounded-xl w-full shadow-lg"
       >
         {buttonText}
       </button>
@@ -380,7 +171,7 @@ function Page() {
                   type="text"
                   value={fromThisCurrency}
                   onChange={(e) => setFromThisCurrency(e.target.value)}
-                  className="w-fit md:w-full  border border-border bg-background rounded-lg p-3 outline-none focus:ring-2 focus:ring-primary/50 transition-all"
+                  className="w-full border border-border bg-background rounded-lg p-3 outline-none focus:ring-2 focus:ring-primary/50"
                   placeholder="e.g. USDT"
                   list="coins-list"
                   required
@@ -420,7 +211,7 @@ function Page() {
 
               <button
                 type="submit"
-                className="bg-primary hover:bg-primary/90 text-white font-bold px-6 py-4 rounded-xl w-full shadow-lg transition-all active:scale-[0.98]"
+                className="bg-primary text-white font-bold px-6 py-4 rounded-xl w-full shadow-lg"
               >
                SWAP ASSETS
               </button>
@@ -441,19 +232,19 @@ function Page() {
       className="bg-background text-foreground w-full min-h-screen p-4 font-bold"
     >
       <div className="max-w-6xl mx-auto">
-        <h1 className="text-3xl mb-8 flex items-center gap-3">
+        <h1 className="text-3xl mb-6 md:mb-8 flex items-center gap-3">
           <ArrowRightLeft className="text-primary" />
           Exchange Assets
         </h1>
 
         <div className="w-full md:w-3/4 mx-auto border border-border bg-secondary/20 rounded-2xl overflow-hidden shadow-2xl min-h-[500px] grid md:grid-cols-[1fr_2fr]">
-          <div className="flex md:flex-col justify-around md:justify-start gap-4 p-6 bg-secondary/40 border-b md:border-b-0 md:border-r border-border">
+          <div className="flex flex-wrap md:flex-col justify-around md:justify-start gap-4 p-4 md:p-6 bg-secondary/40 border-b md:border-b-0 md:border-r border-border">
             <button
               onClick={() => setActiveTab("sell")}
-              className={`flex items-center gap-2 px-6 py-3 rounded-xl transition-all duration-200 ${
+                 className={`flex items-center gap-2 px-6 py-3 rounded-xl ${
                 activeTab === "sell" 
-                  ? "bg-primary text-white shadow-lg scale-105" 
-                  : "bg-background hover:bg-secondary"
+                  ? "bg-primary text-white shadow-lg" 
+                  : "bg-background text-foreground"
               }`}
             >
               <TrendingDown size={20} />
@@ -462,10 +253,10 @@ function Page() {
 
             <button
               onClick={() => setActiveTab("buy")}
-              className={`flex items-center gap-2 px-6 py-3 rounded-xl transition-all duration-200 ${
+               className={`flex items-center gap-2 px-6 py-3 rounded-xl ${
                 activeTab === "buy" 
-                  ? "bg-primary text-white shadow-lg scale-105" 
-                  : "bg-background hover:bg-secondary text-black"
+                  ? "bg-primary text-white shadow-lg" 
+                  : "bg-background text-foreground"
               }`}
             >
               <TrendingUp size={20} />
@@ -474,10 +265,10 @@ function Page() {
 
             <button
               onClick={() => setActiveTab("swap")}
-              className={`flex items-center gap-2 px-6 py-3 rounded-xl transition-all duration-200 ${
+              className={`flex items-center gap-2 px-6 py-3 rounded-xl ${
                 activeTab === "swap" 
-                  ? "bg-primary text-white shadow-lg scale-105" 
-                  : "bg-background hover:bg-secondary"
+                  ? "bg-primary text-white shadow-lg" 
+                  : "bg-background text-foreground"
               }`}
             >
               <ArrowRightLeft size={20} />
@@ -485,7 +276,7 @@ function Page() {
             </button>
           </div>
 
-          <div className="p-8 relative">
+          <div className="p-5 md:p-8 relative">
             {loading && (
               <div className="absolute inset-0 bg-background/50 backdrop-blur-sm flex items-center justify-center z-10 rounded-r-2xl">
                 <Loader2 className="animate-spin text-primary" size={40} />
