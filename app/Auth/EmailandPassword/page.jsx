0e1e3b2fc35ab4da52ddaf01f81137b974b213e1
Hastingamo <hezekiahhastings14@gmail.com>
2026-03-16 04:10:34 +0100

    yyy

--- app/Auth/EmailandPassword/page.jsx
+++ app/Auth/EmailandPassword/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from 'react'
 import EmailDemo from './EmailDemo'
 
@@ -6,6 +7,7 @@ function Page() {
   return (
     <div>
       <Page/>
+      <EmailDemo/>
     </div>
   )
 }
